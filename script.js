10428cab19e0345e7be4aa5a24c4bdf6c6e4892b
GDEtrailrunning <gdeconcienciacorporal@gmail.com>
2025-06-09 02:04:18 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
   actualizarDiasRestantes();
   setInterval(actualizarDiasRestantes, 24*60*60*1000); // Actualizar diario
-   // Datos para el gráfico de líneas
+  //Datos para el gráfico de líneas
 const dataLine1 = {
-  labels: ['Día 1', 'Día 2', 'Dia 3', 'Día 4'], // Etiquetas de los días
+  labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4'], // Etiquetas de los días
   datasets: [{
     label: 'Km recorridos en Semana 23',
     data: [18, 11, 14, 20], // Datos de los km recorridos en cada día
@@ -13,12 +13,11 @@ const dataLine1 = {
   }]
 };
 
-
 const dataLine2 = {
-  labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4'], // Etiquetas de los días
+  labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4'],// Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana 23',
-    data: [12, 24, 12, 29], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 24',
+    data: [17, 19, 8, 26], // Datos de los km recorridos en cada día
     borderColor: 'rgba(153, 102, 255, 1)',
     fill: true,
     tension: 0.1
@@ -110,15 +109,4 @@ function actualizarDiasRestantes() {
   document.getElementById("Semana 2").appendChild(video3);
   document.getElementById("Semana 2").appendChild(video4);
 
-
-  audios.forEach(audio => {
-    audio.addEventListener("play", () => {
-      audios.forEach(otherAudio => {
-        if (otherAudio !== audio) {
-          otherAudio.pause();
-        }
-      });
-    });
-  });
-  
 });
