9e6398bd84047dc0d5d811728cf80810dc490a65
GDEtrailrunning <gdeconcienciacorporal@gmail.com>
2025-06-09 01:53:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -13,11 +13,12 @@ const dataLine1 = {
   }]
 };
 
+
 const dataLine2 = {
-  labels: ['Día 1', 'Día 2', 'Dia 3', 'Día 4'], // Etiquetas de los días
+  labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4'], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana 24',
-    data: [17, 19, 8, 26], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 23',
+    data: [12, 24, 12, 29], // Datos de los km recorridos en cada día
     borderColor: 'rgba(153, 102, 255, 1)',
     fill: true,
     tension: 0.1
@@ -25,7 +26,10 @@ const dataLine2 = {
 };
 
 // Configuración del gráfico de líneas
-const configLine = {
+
+
+// Seleccione los contextos de los <canvas> y cree los gráficos
+const myChartLine1 = new Chart(document.getElementById('myChart-line-1'), {
   type: 'line',
   data: dataLine1, // Puede cambiar esto a dataLine2 para mostrar el gráfico de la segunda sección
   options: {
@@ -35,11 +39,9 @@ const configLine = {
       }
     }
   }
-};
+});
 
-// Seleccione los contextos de los <canvas> y cree los gráficos
-const myChartLine1 = new Chart(document.getElementById('myChart-line-1'), configLine);
-const myChartLine2 = new Chart(document.getElementById('myChart-line-2'), {
+ const myChartLine2 = new Chart(document.getElementById('myChart-line-2'), {
   type: 'line',
   data: dataLine2,
   options: {
@@ -120,4 +122,3 @@ function actualizarDiasRestantes() {
   });
   
 });
-
