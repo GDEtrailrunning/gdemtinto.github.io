04d374a36d542c821252b49c8bcc267e981aeaf1
GDEtrailrunning <gdeconcienciacorporal@gmail.com>
2025-08-25 12:10:11 -0300

    Commit weeks 33 & 34

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ document.addEventListener("DOMContentLoaded", function () {
 const dataLine1 = {
   labels: ['Día 1', 'Día 2', 'Dia 3', 'Día 4'], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana 31',
-    data: [10, 14, 16, 23], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 33',
+    data: [13, 23, 10, 17], // Datos de los km recorridos en cada día
     borderColor: 'rgba(75, 192, 192, 1)',
     fill: true,
     tension: 0.1
@@ -16,8 +16,8 @@ const dataLine1 = {
 const dataLine2 = {
   labels: ['Día 1', 'Día 2', 'Dia 3', 'Dia 4'], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana 32',
-    data: [19, 14, 13, 25], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 34',
+    data: [6, 6, 7, 42], // Datos de los km recorridos en cada día
     borderColor: 'rgba(153, 102, 255, 1)',
     fill: true,
     tension: 0.1
