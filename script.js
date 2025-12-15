37c1a9d2434d6f653664c2a4741911fc4604e89e
GDEtrailrunning <gdeconcienciacorporal@gmail.com>
2025-12-14 22:47:50 -0300

    Subo semana 49 y 50

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ document.addEventListener("DOMContentLoaded", function () {
 const dataLine1 = {
   labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4'], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana 47',
-    data: [12, 14, 10, 18], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 49',
+    data: [13, 12, 17, 25], // Datos de los km recorridos en cada día
     borderColor: 'rgba(75, 192, 192, 1)',
     fill: true,
     tension: 0.1
@@ -16,8 +16,8 @@ const dataLine1 = {
 const dataLine2 = {
   labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4'], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana 48',
-    data: [9, 13, 13, 0], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 50',
+    data: [17, 24, 18, 30], // Datos de los km recorridos en cada día
     borderColor: 'rgba(153, 102, 255, 1)',
     fill: true,
     tension: 0.1
